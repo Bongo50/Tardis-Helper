0d1f829e21bcb3d55197e217052d70cae7a46835
Bongo50 <alfie10@yahoo.com>
2021-07-22 18:59:47 +0100

    Added t?info and autolinking

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_random():
     print("Random page requested:")
     print(page_json)
     page_name = page_json["query"]["random"][0]["title"]
-    page_url = "https://www.tardis.fandom.com/wiki/" + page_name
+    page_url = "https://tardis.fandom.com/wiki/" + page_name
     page = url_ready(page_url)
     return(page)
 
@@ -57,7 +57,7 @@ def search_pages(term, count):
     results = ""
     for n in range(0,int(count)):
         page = results_json["query"]["search"][n]["title"]
-        results += "**"+str(n+1)+".** - "+page+": https://www.tardis.fandom.com/wiki/"+url_ready(page)+'''
+        results += "**"+str(n+1)+".** - "+page+": <https://tardis.fandom.com/wiki/"+url_ready(page)+'''>
 '''
     return(results)
 
@@ -65,16 +65,68 @@ def search_pages(term, count):
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     await bot.change_presence(activity=discord.Game(name="t?help for help"))
+
+@bot.event
+async def on_message(message):
+    #check for uses of [[ and link accordingly
+    if "[[" in message.content:
+        message_list = message.content.split('[[')
+        print(message_list)
+        links=[]
+        for n in range(1,len(message_list)):
+            link_list = message_list[n].split(']]')
+            links.append(url_ready(link_list[0]))
+        print(links)
+        urls = ""
+        for link in links:
+            urls += "<https://tardis.fandom.com/wiki/"+link+'''>
+'''
+        await message.reply(urls)
+
+    #check for uses of {{ and link accordingly
+    if "{{" in message.content:
+        message_list = message.content.split('{{')
+        print(message_list)
+        links=[]
+        for n in range(1,len(message_list)):
+            link_list = message_list[n].split('}}')
+            links.append(url_ready(link_list[0]))
+        print(links)
+        urls = ""
+        for link in links:
+            urls += "<https://tardis.fandom.com/wiki/Template:"+link+'''>
+'''
+        await message.reply(urls)
         
 @bot.command()
 async def ping(ctx):
     await ctx.reply('pong')
+
+@bot.command()
+async def invite(ctx):
+    await ctx.reply('https://discord.com/api/oauth2/authorize?client_id=867699991245619231&permissions=2148002880&scope=bot')
+
+@bot.command()
+async def info(ctx):
+    await ctx.reply('''**== Info Message ==**
+<@867699991245619231> is a bot designed to intergrate some functions of Tardis Data Core, the Doctor Who Wiki, into Discord.
+I was created by <@409330514693193728> (and yes, he gets pinged every time this command is run).
+My prefix is `t?`.
+For help, run `t?help`.
+To invite me, run `t?invite`.
+
+My profile picture is taken from <https://tardis.fandom.com/wiki/File:Favicon.ico>
+Tardis Data Core can be visited at <https://tardis.fandom.com/>.
+
+Tardis Helper is in no way officially affiliated with Tardis Data Core. Bongo50, however, does edit there frequently as [[User:Bongolium500]].''')
     
 @bot.command()
 async def help(ctx):
     await ctx.reply('''**== Help Message ==**
 `t?help` - this message
+`t?info` - get some information about me
 `t?ping` - ping the bot (to see if I'm working
+`t?invite` - get an invite link
 `t?random` - get a random page from the (main) namespace
 `t?contents <page>` - get the contents of any Tardis page - **note: this does not yet work fully**
 `t?search <search query> <number of results>` - search Tardis Data Core. To use a multi-word search query, suround it in `"`''')
